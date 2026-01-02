3ae5e37a6c58dc7be5a69b6bbdc64ace52bd48b5
DeviPrasad84 <devi841997@gmail.com>
2026-01-02 16:52:40 +0530

    opening 1st Demo website to test

---
+++ Base_Test_File.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get("https://demoqa.com/")
+input(" close the browser when click enter ...")
