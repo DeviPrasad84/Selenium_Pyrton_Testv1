a4e3605e5a0dcd4683791f672b419a63116bb757
DeviPrasad84 <devi841997@gmail.com>
2026-01-03 17:02:24 +0530

    just add the new line in hit Hub

--- Base_Test_File.py
+++ Base_Test_File.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
-from selenium import webdriver
 
 driver = webdriver.Chrome()
 driver.maximize_window()
 driver.get("https://demoqa.com/")
-input(" close the browser when click enter ...")
+print("new line")
