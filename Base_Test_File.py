b2e2404cf2c8fb6f3a0288293246e6d46562c499
DeviPrasad84 <devi841997@gmail.com>
2026-01-03 17:25:58 +0530

    Added head less browser

--- Base_Test_File.py
+++ Base_Test_File.py
@@ -1,6 +1,12 @@
 from selenium import webdriver
 
-driver = webdriver.Chrome()
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--disable-gpu")
+options.add_argument("--window-size=1920,1080")
+
+driver = webdriver.Chrome(options=options)
 driver.maximize_window()
 driver.get("https://demoqa.com/")
 print("new line")
+time.sleep(10)
